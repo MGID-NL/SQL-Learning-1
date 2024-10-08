f69f81c2e9e8c723f638a020ba889651055f4bfe
MGID <graventein.m@gmail.com>
2024-10-08 11:14:27 +0200

    Query 3 Done

--- project_sql/3_meest_gewilde_skilss.sql
+++ project_sql/3_meest_gewilde_skilss.sql
@@ -1,4 +1,14 @@
-SELECT *
+SELECT 
+    skills,  
+    COUNT(skills_job_dim.job_id) AS Demand_Count
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_location LIKE '%Netherlands%' AND
+    job_title_short = 'Data Engineer'
+GROUP BY
+    skills
+ORDER BY
+    Demand_Count DESC
+LIMIT 10
