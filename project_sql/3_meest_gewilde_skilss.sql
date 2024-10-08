c87eb480ae67ccfa07405d54c56c308a58919adf
MGID <graventein.m@gmail.com>
2024-10-08 10:50:25 +0200

    Query 3 Start

---
+++ project_sql/3_meest_gewilde_skilss.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
